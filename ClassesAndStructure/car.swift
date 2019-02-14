3ae3dd19185aae34f68126ab72e5991050963a01
MacStudent <macstudent@126-iMac26.local>
2019-02-13 19:37:08 -0500

    closes and access modifies, enum

---
+++ ClassesAndStructure/car.swift
@@ -0,0 +1,57 @@
+//
+//  car.swift
+//  ClassesAndStructure
+//
+//  Created by MacStudent on 2019-02-13.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+internal enum Color { // same project
+    case Red
+    case Blue
+    case black
+    case White
+}
+
+fileprivate class hello { ///same file
+    
+    
+}
+class Car //////// open available to whole world
+{
+    
+    var vin: String
+    var model: String
+    var type: String
+    var color: Color
+    var speed: Float
+    
+    init() {
+        
+        self.vin = String()
+        self.model = String()
+        self.type = String()
+        self.color = .White
+        self.speed = 0.0
+    }
+    
+    func setData(vin: String, model: String, type: String, color: Color, speed: Float)
+    {
+        self.vin = String()
+        self.model = String()
+        self.type = String()
+        self.color = color
+        self.speed = Float()
+    }
+    
+    func display()
+    {
+        print(vin)
+        print(model)
+        print(type)
+        print(color)
+        print(speed)
+    }
+}
--- ClassesAndStructure/main.swift
+++ ClassesAndStructure/main.swift
@@ -43,58 +43,15 @@ var p1 = Person()
 p1.setdata()
 p1.display()
 
-class Student
-{
-   private var pid: Int
-    private var fname: String
-   private var lname: String
-    
-    init() {
-        
-        pid = 0
-        fname = String()
-        lname = String()
-        
-    }
-    init(pid: Int, fname: String, lname: String) {
-        
-        self.pid = pid
-        self.fname = fname
-        self.lname = lname
-    }
-    func display(){
-        print("Student id : \(self.pid)")
-        print("student  first name : \(self.fname)")
-        print("student last name : \(self.lname)")
-        
-    }
-}
+//var s1 = Student()
 
-var s1 = Student()
+//s1.display()
+//let s2 = Student(pid: 200, fname: "gury", lname: "man")
 
-s1.display()
-/*
-s1.pid = 100
-s1.fname = "gur"
-s1.lname = "mangat1"
-*/
-
-s1.display()
-////
-let s2 = Student(pid: 200, fname: "gury", lname: "man")
-let s3 = Student(pid: 300, fname: "gury2", lname: "man2")
-let s4 = Student(pid: 400, fname: "gury3", lname: "man3")
-s2.display()
-
-//var s: [Student] = []
-
-var studentArray = [Student]()
-
-studentArray.append(s2)
-studentArray.append(s3)
-studentArray.append(s4)
-
-for x in studentArray{
-    
-    x.display()
-}
+var c1 = Car()
+c1.vin = "sdfsdgdsgds"
+c1.model = "corola"
+c1.color = Color.Red
+c1.speed = 100.0
+c1.type = "dsfs"
+c1.display()
